f98ccd9e7caca57d59bdaf0687cdcb236dba4691
Chanuka999 <chanukaranditha99@gmail.com>
2026-03-11 09:22:29 +0530

    feat: add ProtectedRoute component for route protection based on user authentication

---
+++ backend/src/utils/roles.js
@@ -0,0 +1,14 @@
+export const ROLES = {
+  SUPER_ADMIN: "super_admin",
+  BRANCH_MANAGER: "branch_manager",
+  DISPATCHER: "dispatcher",
+  RIDER: "rider",
+  CUSTOMER: "customer",
+};
+
+export const STAFF_ROLES = [
+  ROLES.SUPER_ADMIN,
+  ROLES.BRANCH_MANAGER,
+  ROLES.DISPATCHER,
+  ROLES.RIDER,
+];
